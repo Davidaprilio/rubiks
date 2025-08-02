5bd970b2f9dc1b64962cf12ad9d3c513e26b6a47
Davidaprilio <david1.aprilio4@gmail.com>
2025-08-02 20:09:06 +0700

    install three

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import PageOLL from "./pages/oll";
+import Cube from "./pages/cube";
 
 function App() {
 
   return (
-    <main className="bg-gray-900 min-h-screen w-full dark">
-      <h1 className="text-white text-2xl text-center font-semibold">Rubiks</h1>
-      <div className="mt-10 flex justify-center">
-        <PageOLL  />
-      </div>
-    </main>
+      <Cube  />
+    // <main className="bg-gray-900 min-h-screen w-full dark">
+    //   <h1 className="text-white text-2xl text-center font-semibold">Rubiks</h1>
+    //   <div className="mt-10 flex justify-center">
+    //   </div>
+    // </main>
   )
 }
 
